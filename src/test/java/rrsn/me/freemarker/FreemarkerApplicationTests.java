961d8be0d50a1c06f5ec93bdbd04bc80e792c125
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-09 09:45:18 -0800

    start.spring.io with Srping Web and Appache Freemaker

---
+++ src/test/java/rrsn/me/freemarker/FreemarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package rrsn.me.freemarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
